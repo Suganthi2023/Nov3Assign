f9f27d03a12bb2bef63ce3de764dfa19dc5f9b5e
Suganthi2023 <suganthir2000@gmail.com>
2023-12-02 01:38:40 -0600

    Adding js,html and css files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Nov3Assign
+# Nov3Assign By Suganthi R
+
+# BookFinder Web
+This web application is created using the Google Books API. The application is designed to give list of books based on user's selection.
+
+# Design
+The user has two options - Book name or Author name. The user can select their choice using the dropdown menu.Based on the selection, the books will be 
+displayed with title, author and publisher details.
+If the user selects Book option, types the book name in the input field and submits the request using submit button.The books corresponding to the typed
+book name are displayed in the display area. 
+If the user selects Author option, types the author name, the app gives the list of books written by the author in the display area.
+
+When the user types in the next selection, the contents already in the display area gets cleared and the new searched content is displayed. 
+
+# Functionality
+Whenever the submit button is clicked, the eventlistener is triggered and the function inside the eventlistener is executed which uses the async await call back function to do the process of searching the Google Books API and displaying the same for the user. 
--- script.js
+++ script.js
@@ -1,39 +1,109 @@
-const apiKey = 'AIzaSyCVB8GRP_e_JQtiG3oZXPubDnwG_JVbO4A';
-let title = 'Pride and Prejudice';
-let book = {};
+//API Key for accessing Google Books API
+//const apiKey = 'AIzaSyCVB8GRP_e_JQtiG3oZXPubDnwG_JVbO4A'; - Please uncomment this line when working with this code
+//const query = 'Jane Austen'; // User's input ---Used during development
+ // User's selection (either 'title' or 'author') ----Used during development
 
-const fetchAPIData = async () => {
+ //Function to search for books using the Google Books API. 
+const searchBooks = async (query, searchType) => {
     try {
-        const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${title}&key=${apiKey}`
-        const response = await fetch (apiUrl);
+        let apiUrl;
+        if (searchType === 'Book') {
+//            apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`; -- please uncomment this line when working with code
+        } else if (searchType === 'Author') {
+//            apiUrl = `https://www.googleapis.com/books/v1/volumes?q=inauthor:${query}&key=${apiKey}`; --Please uncomment this line when working with code
+        }
+
+        const response = await fetch(apiUrl);
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error('Fetch Eroor', error);
+        console.error('Fetch Error', error);
     }
-}
+};
 
-const printbook = async ()=> {
-    data= await fetchAPIData();
-    if (data.items && data.items.length >0){
-    //    data.items.forEach((bookItem,index)=>{
-    //        const volumeInfo = bookItem.volumeInfo;
-    //        const title = volumeInfo.title;
-    //        const authors = volumeInfo.authors?volumeInfo.authors.join(','):'Unknown Author';
-    //        const publisher = volumeInfo.publisher || 'Unknown Publisher';
+//Used this section during development 
+//const printbook = async ()=> {
+//    data= await searchBooks(query, searchType);
+//    if (data.items && data.items.length >0){
+//       data.items.forEach((bookItem,index)=>{
+//            const volumeInfo = bookItem.volumeInfo;
+//            const title = volumeInfo.title;
+//            const authors = volumeInfo.authors?volumeInfo.authors.join(','):'Unknown Author';
+//            const publisher = volumeInfo.publisher || 'Unknown Publisher';
 
-    //        console.log(`Book ${index+1}:`);
-    //        console.log(`Title:${title}`);
-    //        console.log(`Authors: ${authors}`);
-    //        console.log(`Publisher: ${publisher}`);
-    //        console.log('-------------------------')
-    console.log(data.items);
-     //   })
+//           console.log(`Book ${index+1}:`);
+//            console.log(`Title:${title}`);
+//           console.log(`Authors: ${authors}`);
+//            console.log(`Publisher: ${publisher}`);
+//            console.log('-------------------------')
+    //console.log(data.items);
+//    })
         
-    } else {
-        console.log('No books found');
-    }
-}
-    
+//    } else {
+//        console.log('No books found');
+//    }
+//}
+//printbook();
+//Getting HTML Elements
+const getbutton = document.getElementById('submit');
+const bookdiv = document.getElementById('booklist');
+const bookinfo = document.getElementById('bookinfo');
 
-printbook();
+//Event Listener for the submit button which handles userinput,book search initiation and result display.
+getbutton.addEventListener('click', async (e) =>{
+    e.preventDefault();
+
+    //Get the userinput and selection 
+    const input = document.getElementById('inputname');
+    const inputValue = input.value;
+    const select= document.getElementById('selecttype');
+    const search = select.value;
+   // console.log(inputValue);
+    input.value=''; //clear input field
+
+    //Book search using Google Books API
+    const data = await searchBooks(inputValue,search);
+        //check if books were found
+       if (data.items && data.items.length >0){
+            //check if bookinfo element is not null
+            if(bookinfo){
+                //If any previous content in the display area, clear the display
+                bookinfo.innerHTML = '';
+                // Displaying each book information
+                data.items.forEach((bookItem,index)=>{
+                    const volumeInfo = bookItem.volumeInfo;
+                    const title = volumeInfo.title;
+                    const authors = volumeInfo.authors?volumeInfo.authors.join(','):'Unknown Author';
+                    const publisher = volumeInfo.publisher || 'Unknown Publisher';
+                    //Creating a div for each book
+                    const bookElement = document.createElement('div');
+                    bookElement.innerHTML = `
+                    <p>Book ${index+1}</p>
+                    <p>Title: ${title}</p>
+                    <p>Authors: ${authors}</p>
+                    <p>Publisher: ${publisher}</p>
+                    <br>
+                    `;
+                    //Appending book information to the bookinfo element
+                    bookinfo.appendChild(bookElement);
+                //Used console logs during development
+                // console.log(`Book ${index+1}:`);
+              //  console.log(`Title:${title}`);
+             //  console.log(`Authors: ${authors}`);
+             // console.log(`Publisher: ${publisher}`);
+            //  console.log('-------------------------')
+                //console.log(book);         
+                })
+                //show the bookinfo in the display area
+                if(bookdiv.style.display === 'none'){
+                    bookdiv.style.display = 'block';
+                } else {
+                    console.log('No books found');
+                }
+            
+            }else {
+                console.log('bookinfo is null');
+            }      
+
+       }
+})
